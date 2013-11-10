1aee638d25965461ab5f8b29c215372873d3980d
S1aNT <p.slant@gmail.com>
2013-11-10 14:17:21 +0400

    Create empty project

---
+++ src/com/wordpress/zanyxdev/Posts2Maps/DebugActivity.java
@@ -0,0 +1,15 @@
+package com.wordpress.zanyxdev.Posts2Maps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DebugActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

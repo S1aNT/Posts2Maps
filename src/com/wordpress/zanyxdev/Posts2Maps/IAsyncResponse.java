a416d9a43b33364fef8752e9e4a22e4d44f81f39
S1aNT <p.slant@gmail.com>
2013-11-10 20:19:54 +0400

    Add interface to get AsyncTask result

---
+++ src/com/wordpress/zanyxdev/Posts2Maps/IAsyncResponse.java
@@ -0,0 +1,13 @@
+package com.wordpress.zanyxdev.Posts2Maps;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: slant
+ * Date: 10.11.13
+ * Time: 18:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+public interface IAsyncResponse {
+    void processFinish(Boolean result);
+}
---
+++ src/com/wordpress/zanyxdev/Posts2Maps/tools/ConnectionDetectorTask.java
@@ -0,0 +1,65 @@
+package com.wordpress.zanyxdev.Posts2Maps.tools;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Usage ConnectionDetector cDetectTask = new MyCustomTask(context);
+ * task.execute(..);
+ */
+public class ConnectionDetector extends AsyncTask<Void, Void, Boolean> {
+    private Context mContext;
+    private String LOG_TAG = "Posts2Map::".concat(getClass().getSimpleName());
+
+    public ConnectionDetector(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        // TODO Auto-generated method stub
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        ConnectivityManager connectivity = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo activeInfo = connectivity.getActiveNetworkInfo();
+            if (activeInfo != null && activeInfo.isConnected()) {
+                try {
+                    URL url = new URL("http://www.google.com");
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setConnectTimeout(3000);
+                    urlConnection.connect();
+                    if (urlConnection.getResponseCode() == 200) {
+                        Log.v(LOG_TAG, "ONLINE");
+
+                        return true;
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Log.v(LOG_TAG, "OFFLINE");
+        return false;
+    }
+
+    //other methods like onPreExecute etc.
+    protected void onPostExecute(Boolean result) {
+        Log.v(LOG_TAG, "onPostExecute result:".concat(result.toString()));
+        super.onPostExecute(result);
+    }
+
+}
